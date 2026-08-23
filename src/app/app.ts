24409fe26c0548a3005f6db0062b60aab5a2e357
Huiyu Li <curlyuu@gmail.com>
2026-08-23 23:04:20 +0800

    feat: Add counter functionality with increment, decrement, and reset buttons

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,17 @@ import { Login } from './login/login';
 export class App {
   protected readonly title = signal('angular-learning-notes');
   version=21
+  count=0
+
+  handleIncrement() {
+    this.count++;
+  }
+
+  handleDecrement() {
+    this.count--;
+  }
+
+  handleReset() {
+    this.count=0;
+  }
 }
